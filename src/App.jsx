be1d3f07f23ba5916a6bf1eb29159d81c92278e9
jkl <alexwang925@gmail.com>
2024-03-26 01:47:23 -0300

    third tab done

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,40 @@ import {puppyList} from './data.js';
 import {useState} from 'react';
 
 function App() {
-  console.log(puppyList);
+
   const [puppies, setPuppies] = useState(puppyList);
-  console.log(puppies);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  const featuredPup = puppies.find(pup => pup.id === featPupId);
+
   return (
     <>
+    
       {
         puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>
+          return (
+            <p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id}>
+              {puppy.name}
+            </p>
+          )
         })
       }
+
+      {
+        featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )
+      }
+
     </>
   );
+
 }
 
 export default App;
