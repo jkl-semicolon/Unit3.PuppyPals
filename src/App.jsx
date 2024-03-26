b01485f7b539323fac96e18d4c643d49adea279e
jkl <alexwang925@gmail.com>
2024-03-26 01:20:32 -0300

    second tab complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import './App.css'
+import './App.css';
+import {puppyList} from './data.js';
+import {useState} from 'react';
 
 function App() {
-
+  console.log(puppyList);
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppies);
   return (
     <>
+      {
+        puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>
+        })
+      }
     </>
-  )
+  );
 }
 
-export default App
+export default App;
